763065270adde859fac8a410a252216049793854
Mario <netrix4@gmail.com>
2025-10-17 19:12:59 -0700

    Add 2 demos

---
+++ ocr.py
@@ -0,0 +1,27 @@
+# import pytesseract
+# from PIL import Image
+
+# # Open an image file
+# img = Image.open('/home/mario/Documents/ITE2025-2/BackEnd2/JuegoBackend/superC.png')
+
+# # Use pytesseract to extract text
+# text = pytesseract.image_to_string(img)
+# print(text)
+
+
+import easyocr
+from PIL import Image
+
+# Initialize the OCR reader (specify languages)
+reader = easyocr.Reader(['en'])
+
+# Read text from an image
+results = reader.readtext('./superC.png')
+
+for (bbox, text, prob) in results:
+    print(f"Detected text: {text}, Confidence: {prob:.2f}")
+    # print(f"Detected text: {text}")
+
+
+img = Image.open('./superC.png')
+img.show('title')
